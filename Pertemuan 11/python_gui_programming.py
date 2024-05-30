89cafa3047bdcb09bbab1a56abea740466f94976
Xenodrom31 <revo.rahmat@student.upj.ac.id>
2024-05-30 08:08:03 +0700

    feat: Add GUI examples using Tkinter

---
+++ Pertemuan 11/python_gui_programming.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import simpledialog
+
+root = tk.Tk()
+data = [
+    ['Satrio', 20, 'Jakarta'],
+    ['Azkaa', 19,  'Jombang'],
+    ['Maria', 21, 'Bukit Tinggi']
+]
+
